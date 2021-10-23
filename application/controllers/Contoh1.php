97e9b17eb2f9459081dd7d9a0ad29332293b4543
aqluzd <dzulqafauz@gmail.com>
2021-10-23 20:39:12 +0700

    Menambahkan Contoh1.php untuk tugas 1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Dzulqa Fauzan Abdillah
+            Saya tinggal di daerah Ciamis
+            olah raga yang saya sukai adalah
+            Basket";
+    }
+}
