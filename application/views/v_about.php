fb0dd4d6da8d592e75ddf06e86ff18050cea097a
aqluzd <dzulqafauz@gmail.com>
2021-11-06 07:29:44 +0700

    Menambahkan pertemuan 5 dan 6 untuk tugas Kelompok

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Dzulqa Fauzan</li>
+ <li>Nama Belakang : Abdillah</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Ciamis</li>
+ </ul>
+
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Tasikmalaya</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Basket</li>
+ <li>Bersepeda</li>
+ </ul>
+</section>
